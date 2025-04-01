7512a2413b372356fa53599eea753281cdabe1a0
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-04-01 09:59:40 -0400

    Make prompts optional

--- addons/editor-cleanup-plus/userscript.js
+++ addons/editor-cleanup-plus/userscript.js
@@ -18,6 +18,7 @@ export default async function({ addon, console, msg, safeMsg: m }) {
 
   const doCleanUp = () => {
     const workspace = addon.tab.traps.getWorkspace();
+    const promptUnused = addon.settings.get("promptUnused");
 
     UndoGroup.startUndoGroup(workspace);
 
@@ -28,7 +29,7 @@ export default async function({ addon, console, msg, safeMsg: m }) {
       const message = msg("orphaned", {
         count: orphanCount,
       });
-      if (confirm(message)) {
+      if (promptUnused && confirm(message)) {
         for (const block of result.orphans.blocks) {
           block.dispose();
         }
@@ -79,58 +80,63 @@ export default async function({ addon, console, msg, safeMsg: m }) {
       }
     }
 
-    setTimeout(() => {
-      // Locate unused local variables...
-      const map = workspace.getVariableMap();
-      const vars = map.getVariablesOfType("");
-      const unusedVariables = [];
+    if (promptUnused) {
+      setTimeout(promptUnusedVariabled, 100);
+    }
 
-      for (const row of vars) {
-        if (row.isLocal) {
-          const usages = getVariableUsesById(row.getId(), workspace);
-          if (!usages || usages.length === 0) {
-            unusedVariables.push(row);
-          }
-        }
-      }
-
-      if (unusedVariables.length > 0) {
-        const message = msg("unused-var", {
-          count: unusedVariables.length,
-          variables: unusedVariables.map((x) => x.name).join(", "),
-        });
-        if (confirm(message)) {
-          for (const orphan of unusedVariables) {
-            workspace.deleteVariableById(orphan.getId());
-          }
-        }
-      }
-
-      // Locate unused local lists...
-      const lists = map.getVariablesOfType("list");
-      const unusedLists = [];
-
-      for (const row of lists) {
-        if (row.isLocal) {
-          const usages = getVariableUsesById(row.getId(), workspace);
-          if (!usages || usages.length === 0) {
-            unusedLists.push(row);
-          }
-        }
-      }
-      if (unusedLists.length > 0) {
-        const message = msg("unused-list", {
-          count: unusedLists.length,
-          lists: unusedLists.map((x) => x.name).join(", "),
-        });
-        if (confirm(message)) {
-          for (const orphan of unusedLists) {
-            workspace.deleteVariableById(orphan.getId());
-          }
-        }
-      }
-
-      UndoGroup.endUndoGroup(workspace);
-    }, 100);
+    UndoGroup.endUndoGroup(workspace);
   };
+
+  function promptUnusedVariabled() {
+    const workspace = addon.tab.traps.getWorkspace();
+    // Locate unused local variables...
+    const map = workspace.getVariableMap();
+    const vars = map.getVariablesOfType("");
+    const unusedVariables = [];
+
+    for (const row of vars) {
+      if (row.isLocal) {
+        const usages = getVariableUsesById(row.getId(), workspace);
+        if (!usages || usages.length === 0) {
+          unusedVariables.push(row);
+        }
+      }
+    }
+
+    if (unusedVariables.length > 0) {
+      const message = msg("unused-var", {
+        count: unusedVariables.length,
+        variables: unusedVariables.map((x) => x.name).join(", "),
+      });
+      if (confirm(message)) {
+        for (const orphan of unusedVariables) {
+          workspace.deleteVariableById(orphan.getId());
+        }
+      }
+    }
+
+    // Locate unused local lists...
+    const lists = map.getVariablesOfType("list");
+    const unusedLists = [];
+
+    for (const row of lists) {
+      if (row.isLocal) {
+        const usages = getVariableUsesById(row.getId(), workspace);
+        if (!usages || usages.length === 0) {
+          unusedLists.push(row);
+        }
+      }
+    }
+    if (unusedLists.length > 0) {
+      const message = msg("unused-list", {
+        count: unusedLists.length,
+        lists: unusedLists.map((x) => x.name).join(", "),
+      });
+      if (confirm(message)) {
+        for (const orphan of unusedLists) {
+          workspace.deleteVariableById(orphan.getId());
+        }
+      }
+    }
+  }
 }

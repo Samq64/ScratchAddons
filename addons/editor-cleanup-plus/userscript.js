ce1ebef0f367d41cb4c2625c6a5ca349b7bad96b
Samq64 <81489795+Samq64@users.noreply.github.com>
2025-04-01 09:43:30 -0400

    Refactor

--- addons/editor-cleanup-plus/userscript.js
+++ addons/editor-cleanup-plus/userscript.js
@@ -1,32 +1,31 @@
 import UndoGroup from "../../libraries/common/cs/UndoGroup.js";
 import { getVariableUsesById, getOrderedTopBlockColumns } from "../../libraries/common/cs/devtools-utils.js";
 
-export default async function ({ addon, console, msg, safeMsg: m }) {
+export default async function({ addon, console, msg, safeMsg: m }) {
   const blockly = await addon.tab.traps.getBlockly();
   const vm = addon.tab.traps.vm;
-  const getWorkspace = () => addon.tab.traps.getWorkspace();
 
-  let originalMsg = blockly.Msg.CLEAN_UP;
+  const originalMsg = blockly.Msg.CLEAN_UP;
   addon.self.addEventListener("disabled", () => (blockly.Msg.CLEAN_UP = originalMsg));
   addon.self.addEventListener("reenabled", () => (blockly.Msg.CLEAN_UP = m("clean-plus")));
   blockly.Msg.CLEAN_UP = m("clean-plus");
 
   const oldCleanUpFunc = blockly.WorkspaceSvg.prototype.cleanUp;
-  blockly.WorkspaceSvg.prototype.cleanUp = function () {
+  blockly.WorkspaceSvg.prototype.cleanUp = function() {
     if (addon.self.disabled) return oldCleanUpFunc.call(this);
     doCleanUp();
   };
 
   const doCleanUp = () => {
-    let workspace = getWorkspace();
+    const workspace = addon.tab.traps.getWorkspace();
 
     UndoGroup.startUndoGroup(workspace);
 
-    let result = getOrderedTopBlockColumns(true, workspace);
-    let columns = result.cols;
-    let orphanCount = result.orphans.blocks.length;
+    const result = getOrderedTopBlockColumns(true, workspace);
+    const columns = result.cols;
+    const orphanCount = result.orphans.blocks.length;
     if (orphanCount > 0) {
-      let message = msg("orphaned", {
+      const message = msg("orphaned", {
         count: orphanCount,
       });
       if (confirm(message)) {
@@ -44,31 +43,31 @@ export default async function ({ addon, console, msg, safeMsg: m }) {
     // but script-snap snaps to them
     let cursorX = gridSize / 2;
 
-    let maxWidths = result.maxWidths;
+    const maxWidths = result.maxWidths;
 
     for (const column of columns) {
       let cursorY = gridSize / 2;
       let maxWidth = 0;
 
       for (const block of column.blocks) {
-        let xy = block.getRelativeToSurfaceXY();
+        const xy = block.getRelativeToSurfaceXY();
         if (cursorX - xy.x !== 0 || cursorY - xy.y !== 0) {
           block.moveBy(cursorX - xy.x, cursorY - xy.y);
         }
-        let heightWidth = block.getHeightWidth();
+        const heightWidth = block.getHeightWidth();
         cursorY += heightWidth.height + gridSize;
-        cursorY += gridSize - (cursorY + gridSize / 2) % gridSize;
+        cursorY += gridSize - ((cursorY + gridSize / 2) % gridSize);
 
-        let maxWidthWithComments = maxWidths[block.id] || 0;
+        const maxWidthWithComments = maxWidths[block.id] || 0;
         maxWidth = Math.max(maxWidth, Math.max(heightWidth.width, maxWidthWithComments));
       }
 
       cursorX += maxWidth + gridSize;
-      cursorX += gridSize - (cursorX + gridSize / 2) % gridSize;
+      cursorX += gridSize - ((cursorX + gridSize / 2) % gridSize);
     }
 
     // TODO: New Blockly
-    let topComments = workspace.getTopComments();
+    const topComments = workspace.getTopComments();
     for (const comment of topComments) {
       if (comment.setVisible) {
         comment.setVisible(false);
@@ -82,63 +81,48 @@ export default async function ({ addon, console, msg, safeMsg: m }) {
 
     setTimeout(() => {
       // Locate unused local variables...
-      let workspace = getWorkspace();
-      let map = workspace.getVariableMap();
-      let vars = map.getVariablesOfType("");
-      let unusedLocals = [];
+      const map = workspace.getVariableMap();
+      const vars = map.getVariablesOfType("");
+      const unusedVariables = [];
 
       for (const row of vars) {
         if (row.isLocal) {
-          let usages = getVariableUsesById(row.getId(), workspace);
+          const usages = getVariableUsesById(row.getId(), workspace);
           if (!usages || usages.length === 0) {
-            unusedLocals.push(row);
+            unusedVariables.push(row);
           }
         }
       }
 
-      if (unusedLocals.length > 0) {
-        const unusedCount = unusedLocals.length;
-        let message = msg("unused-var", {
-          count: unusedCount,
+      if (unusedVariables.length > 0) {
+        const message = msg("unused-var", {
+          count: unusedVariables.length,
+          variables: unusedVariables.map((x) => x.name).join(", "),
         });
-        for (let i = 0; i < unusedLocals.length; i++) {
-          let orphan = unusedLocals[i];
-          if (i > 0) {
-            message += ", ";
-          }
-          message += orphan.name;
-        }
         if (confirm(message)) {
-          for (const orphan of unusedLocals) {
+          for (const orphan of unusedVariables) {
             workspace.deleteVariableById(orphan.getId());
           }
         }
       }
 
       // Locate unused local lists...
-      let lists = map.getVariablesOfType("list");
-      let unusedLists = [];
+      const lists = map.getVariablesOfType("list");
+      const unusedLists = [];
 
       for (const row of lists) {
         if (row.isLocal) {
-          let usages = getVariableUsesById(row.getId(), workspace);
+          const usages = getVariableUsesById(row.getId(), workspace);
           if (!usages || usages.length === 0) {
             unusedLists.push(row);
           }
         }
       }
       if (unusedLists.length > 0) {
-        const unusedCount = unusedLists.length;
-        let message = msg("unused-list", {
-          count: unusedCount,
+        const message = msg("unused-list", {
+          count: unusedLists.length,
+          lists: unusedLists.map((x) => x.name).join(", "),
         });
-        for (let i = 0; i < unusedLists.length; i++) {
-          let orphan = unusedLists[i];
-          if (i > 0) {
-            message += ", ";
-          }
-          message += orphan.name;
-        }
         if (confirm(message)) {
           for (const orphan of unusedLists) {
             workspace.deleteVariableById(orphan.getId());
